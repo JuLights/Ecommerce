d26aa05564d49921bd4793c0a5da0e9d6e82bcad
David Jugeli <davidjugeli23@gmail.com>
2026-03-20 08:11:27 +0400

    Added CRM essential controllers

---
+++ CRM.API/Controllers/MarketingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketingController : ControllerBase
+    {
+
+    }
+}
